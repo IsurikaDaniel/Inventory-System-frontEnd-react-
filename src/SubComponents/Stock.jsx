e35b901f53bcabd97a0bab105f819fca780a0361
IsurikaDaniel <d.nethmiisurika@gmail.com>
2025-05-02 16:49:24 +0530

    stoke table added

--- src/SubComponents/Stock.jsx
+++ src/SubComponents/Stock.jsx
@@ -5,14 +5,84 @@ import "../SubComponents/Stock.css"; // Confirm path is correct
 import SideBar from "../common/SideBar"; // Adjusted path if needed
 
 function Stock() {
-  return (
-    <div className="d-flex">
-      <SideBar />
-      <div className="stockContainer p-3">
-        hi
-      </div>
-    </div>
-  );
+    return (
+        <div className="d-flex">
+            <SideBar />
+            <div className="stockContainer p-3">
+                <table className="styled-table">
+                    <thead>
+                        <tr>
+                            <th>Order ID</th>
+                            <th>Product</th>
+                            <th>Category</th>
+                            <th>Sales channel</th>
+                            <th>Instruction</th>
+                            <th>Items</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>001</td>
+                            <td>Inverter</td>
+                            <td>cat1</td>
+                            <td>Store name</td>
+                            <td>Stock adjustment</td>
+                            <td>80/100</td>
+                            <td>Completed</td>
+                        </tr>
+                        <tr classNamelass="active-row">
+                            <td>002</td>
+                            <td>Inverter</td>
+                            <td>cat1</td>
+                            <td>Store name</td>
+                            <td>Stock adjustment</td>
+                            <td>80/100</td>
+                            <td>Completed</td>
+                        </tr>
+                        <tr>
+                            <td>003</td>
+                            <td>Inverter</td>
+                            <td>cat1</td>
+                            <td>Store name</td>
+                            <td>Stock adjustment</td>
+                            <td>80/100</td>
+                            <td>Completed</td>
+                        </tr>
+                        <tr>
+                            <td>004</td>
+                            <td>Inverter</td>
+                            <td>cat1</td>
+                            <td>Store name</td>
+                            <td>Stock adjustment</td>
+                            <td>80/100</td>
+                            <td>Completed</td>
+                        </tr>
+                        <tr>
+                            <td>005</td>
+                            <td>Inverter</td>
+                            <td>cat1</td>
+                            <td>Store name</td>
+                            <td>Stock adjustment</td>
+                            <td>80/100</td>
+                            <td>Completed</td>
+                        </tr>
+                        <tr>
+                            <td>006</td>
+                            <td>Inverter</td>
+                            <td>cat1</td>
+                            <td>Store name</td>
+                            <td>Stock adjustment</td>
+                            <td>80/100</td>
+                            <td>Completed</td>
+                        </tr>
+
+                    </tbody>
+                </table>
+
+            </div>
+        </div>
+    );
 }
 
 export default Stock;
